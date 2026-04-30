05d5526bb37454f6b7d612481a953b942add5094
sps8200-hue <sps8200@gmail.com>
2026-04-30 12:55:07 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // 깃허브 저장소 이름을 정확히 넣어주어 주소 경로를 맞춥니다.
   base: '/t-autodoor/', 
-  build: {
-    // 빌드된 파일들이 저장될 위치를 명시합니다.
-    outDir: 'dist',
-  }
 })
