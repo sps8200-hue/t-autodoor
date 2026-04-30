18a64a9855edabf8c6e5d260dca23093564a79c7
sps8200-hue <sps8200@gmail.com>
2026-04-30 12:30:44 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- 이 부분을 '/t-autodoor/' 대신 './' 로 바꿔보세요. 
+  base: './', // <-- 이 부분이 핵심입니다. 모든 파일을 현재 위치에서 찾게 합니다.
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
